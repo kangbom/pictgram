107816cb921801276d31889b5060062472215a43
kangbom <kimkangbom@gmail.com>
2019-09-29 03:16:48 +0000

    validates password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-zA-Z\d]{8,32}+\z/i}
+    
+    has_secure_password
 end
