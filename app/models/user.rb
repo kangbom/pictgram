acc4b872d7861810683b7ba8533db83651bab4aa
kangbom <kimkangbom@gmail.com>
2019-10-01 02:08:57 +0000

    rails 04

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true
-    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-zA-Z\d]{8,32}+\z/i}
+    validates :name, presence: true, length: {maximum: 15}
+    validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i}
     
     has_secure_password
 end
