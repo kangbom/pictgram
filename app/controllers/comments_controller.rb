d744a46161cf953b019bc968f12ec4e49def3105
kangbom <kimkangbom@gmail.com>
2019-10-20 04:38:10 +0000

    change redirect page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
     # binding.pry
     @comment = Comment.new(content: params[:comment][:content], topic_id: params[:comment][:topic_id])
     if @comment.save
-      redirect_to favorites_index_path, success: "コメントが投稿されました。"
+      redirect_to topix_path, success: "コメントが投稿されました。"
     else
       flash.now[:danger] = "コメントの投稿に失敗しました。"
       render :new
